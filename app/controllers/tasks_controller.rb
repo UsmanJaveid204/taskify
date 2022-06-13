e6ab8f8a525b75dcf4e41ebf3e4bcbd6536ffb5b
Usman Javed <usmanjaveid.185203@gmail.com>
2022-06-13 14:58:14 +0500

    change index html file for index action and add set task method in before action for task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
+  before_action :set_task
+
   def index
     @tasks = Task.all
+  end
+
+  private
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
+    @task ||= Task.new
   end
 end
