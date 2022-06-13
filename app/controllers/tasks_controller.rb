3c1ecc35de9586feaf060aeece63c848366d89d7
Usman Javed <usmanjaveid.185203@gmail.com>
2022-06-13 15:48:40 +0500

    set create actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,10 +5,25 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def create
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: "Task was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_task
     @task = Task.find_by(id: params[:id])
-    @task ||= Task.new
+    @task ||= Task.new(task_params)
+  end
+
+  def task_params
+    return if  params[:task].blank?
+    params.require(:task).permit(:description)
   end
 end
